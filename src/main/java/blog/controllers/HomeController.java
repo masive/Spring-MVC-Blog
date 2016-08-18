272ad6f176155a0b0c912586f42f79fbccfad146
Pavel Anchev <masive@abv.bg>
2016-08-18 17:12:12 +0300

    Implement Notifications System

--- src/main/java/blog/controllers/HomeController.java
+++ src/main/java/blog/controllers/HomeController.java
@@ -19,6 +19,7 @@ public class HomeController {
     public String index(Model model) {
         List<Post> latest5Posts = postService.findLatest5();
         model.addAttribute("latest5posts", latest5Posts);
+
         List<Post> latest3Posts = latest5Posts.stream()
                 .limit(3).collect(Collectors.toList());
         model.addAttribute("latest3posts", latest3Posts);
