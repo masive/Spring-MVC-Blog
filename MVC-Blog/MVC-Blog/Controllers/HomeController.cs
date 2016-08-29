7ac3c959dbd16e98864f1f7cbac26d8f90c332db
Mariana Angelova <Mariana Angelova>
2016-08-30 01:13:00 +0300

    Mvc-Blog(C#)

---
+++ MVC-Blog/MVC-Blog/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using MVC_Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ApplicationDbContext();
+            var posts = db.Posts.OrderByDescending(p => p.Date).Take(3);
+            return View(posts.ToList());
+        }
+
+    }
+}
